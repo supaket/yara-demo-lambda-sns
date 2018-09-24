8b7194adf118de62426be2edd080a3e5d0f43a95
Korachal Phadvibulya <boatmef@gmail.com>
2018-09-25 00:31:59 +0700

    Updated updated.txt when put new file to original folder

--- handler.js
+++ handler.js
@@ -1,5 +1,5 @@
-const AWS = require('aws-sdk')
-var s3 = new AWS.S3()
+const aws = require('aws-sdk')
+var s3 = new aws.S3()
 
 module.exports.hello = async (event, context) => {
   return {
@@ -16,11 +16,22 @@ module.exports.hello = async (event, context) => {
 
 module.exports.s3event = (event, context, cb) => {
   console.log('s3event', event)
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 's3event response',
-      input: event,
-    }),
-  }
+  const record = event.Records[0];
+  const bucket = record.s3.bucket.name
+  const originalFilePath = record.s3.object.key
+  console.log('detail', record, bucket, originalFilePath)
+  s3.putObject({
+    Body: originalFilePath,
+    Bucket: bucket,
+    Key: "updated.txt"
+  }, (err, data) => {
+    console.log('put', err, data)
+    cb(err, {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: 's3event response',
+        input: event,
+      })
+    })
+  })
 }
